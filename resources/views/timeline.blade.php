859108c900b8c5538c514882d903882f236c50c3
AsmitNepali <mail2asmitnepali@gmail.com>
2026-06-13 00:11:19 +0545

    Center timeline connector line on entry icons

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -32,7 +32,7 @@
                     @foreach ($groupEntries as $i => $entry)
                         <li class="relative" wire:key="timeline-entry-{{ $entry->id }}">
                             @if ($i !== count($groupEntries) - 1)
-                                <span aria-hidden="true" class="absolute left-[22px] top-9 bottom-0 w-px bg-gray-200 dark:bg-white/10"></span>
+                                <span aria-hidden="true" class="absolute left-[22px] top-9 bottom-0 w-px -translate-x-1/2 bg-gray-200 dark:bg-white/10"></span>
                             @endif
                             {!! $registry->resolve($entry)->render($entry) !!}
                         </li>
@@ -45,7 +45,7 @@
             @foreach ($entries as $i => $entry)
                 <li class="relative" wire:key="timeline-entry-{{ $entry->id }}">
                     @if ($i !== count($entries) - 1)
-                        <span aria-hidden="true" class="absolute left-[22px] top-9 bottom-0 w-px bg-gray-200 dark:bg-white/10"></span>
+                        <span aria-hidden="true" class="absolute left-[22px] top-9 bottom-0 w-px -translate-x-1/2 bg-gray-200 dark:bg-white/10"></span>
                     @endif
                     {!! $registry->resolve($entry)->render($entry) !!}
                 </li>
